7096ac3e71694b42164b505e226739510486ade3
gorimentorin <gori.mentorin@gmail.com>
2014-03-24 12:01:07 -0300

    Se corrigio error variable sin nombre que no permitia guardar en BD Se agregan tablas para guardar resultado busqueda

--- googleImages.php
+++ googleImages.php
@@ -11,20 +11,28 @@ class GoogleImages extends DownloadURl{
 		$turl=sprintf($this->url,$this->userApi,urlencode($this->userKey ),$buscar);
 		$this->getUrl($turl);
 
-		file_put_contents("gat.json", $this->BODY);
-		die();
-
+		//file_put_contents("gat.json", $this->BODY);
+		//die();
+		//$this->BODY=file_get_contents("gat.json");
 		$lstResultados=json_decode($this->BODY,true);
 		$db=new DBInfoGoogleImages(); 
+		$dbBusqueda=new DBbusquedagoogleimages();
+		$dbResultado=new DBresultadobusquedagoogleimages();
+		$Busqueda=new busquedagoogleimages();
+		$Busqueda->busqueda=$buscar;
+		$dbBusqueda->save($Busqueda);
+
 		foreach ($lstResultados["results"]["listado"] as $item ) {
+			$resultado=new resultadobusquedagoogleimages();
 			$info=$item["imagen"];
 
 			$InfoG=new InfoGoogleImages();
 			if(isset($info["src"])){
 				$src=$info["src"];
 				$src=substr($src,strpos($src,"data:image"));
-				$mime=substr($src,0,strpos($src,";"));
-				$InfoG->data=$src;
+				//$src=substr($src,strpos($src,",")+1);
+				$mime=substr($src,strpos($src,":")+1,strpos($src,";")-strpos($src,":")-1);
+				$InfoG->data=substr($src,strpos($src,",")+1);;
 			}
 			else{
 				$InfoG->datasrc=$info["data-src"];
@@ -37,9 +45,11 @@ class GoogleImages extends DownloadURl{
 			$InfoG->hrefSite=$temp["imgrefurl"];
 			$InfoG->hrefImg=$temp["imgurl"];
 			$InfoG->w=$temp["w"];
-			$InfoG->h=$temp["h"];
-			//print_r($infoG)
-			$db->save($infoG);
+			$InfoG->h=$temp["h"];;
+			$db->save($InfoG);
+			$resultado->id_busqueda=$Busqueda->id;
+			$resultado->id_image=$InfoG->id;
+			$dbResultado->save($resultado);
 		}
 
 	}
@@ -62,14 +72,84 @@ class DBInfoGoogleImages extends DBBase{
         {
             if ($this->connect())
             {
-                $sql="insert into googleImages (data, hrefImg, hrefSite, w, h, mime) values  (?,?,?,?,?,?)";
+                $sql="insert into googleImages (data, hrefImg, hrefSite, w, h, mime,`data-src`) values  (?,?,?,?,?,?,?)";
                 if ($stmt = $this->mysqli->prepare($sql))
                 {
-                    $stmt->bind_param("ssssss", $info->data, $info->hrefImg, $info->hrefSite,$info->w,$info->h,$info->mime);
+                    $stmt->bind_param("sssssss", $info->data, $info->hrefImg, $info->hrefSite,$info->w,$info->h,$info->mime,$info->datasrc);
                     $res = $stmt->execute();
                     if($res){
                     	$info->id= $this->mysqli->insert_id;
                     	trace("Imagen  " . $info->id . " guardado en BD con resultado " . $res);
+                    }
+                    else trace("Error  " . $this->mysqli->error);
+                    return $res;
+                }else trace("error prepare".$this->mysqli->error);
+            }else trace("error conn".$this->mysqli->error);
+        }
+        catch (Exception $e)
+        {
+            echo 'Excepción capturada: ', $e->getMessage(), "\n";
+        }
+        return false;
+
+}
+}
+class busquedagoogleimages{
+	public $id;
+	public $busqueda;
+
+}
+
+class DBbusquedagoogleimages extends DBBase{
+	function save(&$info){
+		
+        try
+        {
+            if ($this->connect())
+            {
+                $sql="replace into busquedagoogleimages (busqueda) values  (?)";
+                if ($stmt = $this->mysqli->prepare($sql))
+                {
+                    $stmt->bind_param("s", $info->busqueda);
+                    $res = $stmt->execute();
+                    if($res){
+                    	$info->id= $this->mysqli->insert_id;
+                    	trace("Imagen  " . $info->id . " guardado en BD con resultado " . $res);
+                    }
+                    else trace("Error  " . $this->mysqli->error);
+                    return $res;
+                }else trace("error prepare".$this->mysqli->error);
+            }else trace("error conn".$this->mysqli->error);
+        }
+        catch (Exception $e)
+        {
+            echo 'Excepción capturada: ', $e->getMessage(), "\n";
+        }
+        return false;
+
+}
+}
+class resultadobusquedagoogleimages{
+	public $id_busqueda;
+	public $id_image;
+
+}
+
+class DBresultadobusquedagoogleimages extends DBBase{
+	function save(&$info){
+		
+        try
+        {
+            if ($this->connect())
+            {
+                $sql="replace into resultadobusquedagoogleimages (id_busqueda,id_image) values  (?,?)";
+                if ($stmt = $this->mysqli->prepare($sql))
+                {
+
+                    $stmt->bind_param("ss", $info->id_busqueda,$info->id_image);
+                    $res = $stmt->execute();
+                    if($res){
+                    	trace("Resultado guardado en BD con resultado " . $res);
                     }
                     else trace("Error  " . $this->mysqli->error);
                     return $res;
